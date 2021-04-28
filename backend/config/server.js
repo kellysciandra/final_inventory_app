8a43340988d1deec6a111c52e4620c754531bd00
kellysciandra <kellysciandra@icloud.com>
2021-04-28 17:05:20 -0400

    first

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5713934e1290ba0cf5d9bb8201092d1b'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a6d21fc2-164a-492b-b4e1-4904b860a785'
+};
